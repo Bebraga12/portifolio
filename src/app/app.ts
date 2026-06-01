1f5057c8fcefa9c21dfcfeb1f6a5a10f5e4026da
Bernardo Braga <bebraga12@gmail.com>
2026-06-01 01:54:29 -0300

    fix: switch to static output mode and signal-based popup close
    - outputMode: server -> static (removes SSR hydration on GitHub Pages)
    - App closes popup via effect() watching langService.isSelected()
      instead of output() event (avoids hydration event replay issues)

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,4 +1,5 @@
-import { Component, signal } from '@angular/core';
+import { Component, signal, effect, inject } from '@angular/core';
+import { LanguageService } from './services/language.service';
 import { LanguagePopupComponent } from './components/language-popup/language-popup.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { HeroComponent } from './components/hero/hero.component';
@@ -25,5 +26,14 @@ import { FooterComponent } from './components/footer/footer.component';
   styleUrl: './app.scss',
 })
 export class App {
+  private readonly langService = inject(LanguageService);
   readonly showLangPopup = signal(true);
+
+  constructor() {
+    effect(() => {
+      if (this.langService.isSelected()) {
+        this.showLangPopup.set(false);
+      }
+    });
+  }
 }
